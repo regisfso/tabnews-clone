00071e60ab39fd01fd8b73e7fc8f169abe958639
regisfso <regis.fs.2008@gmail.com>
2024-04-05 22:20:40 +0000

    sacanagem com o amiguinho

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Verônica, eu amo você, se você me ama, da uma risadinha!</h1>
+    return <h1>Roni, se você da a bundinha, da uma risadinha!</h1>
 }
 
 export default Home;
